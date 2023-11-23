4f533919fd4c2309b336cea9b3853a6b82314bd9
fabballe <fabballe@talend.com>
2023-11-23 17:14:03 +0100

    chore(TDP-12585): move TCompV0 to SB3

--- components/components-azurestorage/src/test/java/org/talend/components/azurestorage/AzureStorageGenericBase.java
+++ components/components-azurestorage/src/test/java/org/talend/components/azurestorage/AzureStorageGenericBase.java
@@ -12,7 +12,6 @@
 // ============================================================================
 package org.talend.components.azurestorage;
 
-import jakarta.inject.Inject;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
@@ -45,14 +44,12 @@ public abstract class AzureStorageGenericBase extends AbstractComponentTest2 {
     @Rule
     public ErrorCollector errorCollector = new ErrorCollector();
 
-    @Inject
     DefinitionRegistry testComponentRegistry;
 
     @Override
     public DefinitionRegistryService getDefinitionRegistry() {
         if (testComponentRegistry == null) {
             testComponentRegistry = new DefinitionRegistry();
-
             testComponentRegistry.registerComponentFamilyDefinition(new AzureStorageFamilyDefinition());
         }
         return testComponentRegistry;
--- components/components-fileio/simplefileio-definition/src/test/java/org/talend/components/simplefileio/s3/S3TestIT.java
+++ components/components-fileio/simplefileio-definition/src/test/java/org/talend/components/simplefileio/s3/S3TestIT.java
@@ -12,6 +12,7 @@
 // ============================================================================
 package org.talend.components.simplefileio.s3;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,6 +20,8 @@ import org.talend.components.service.spring.SpringTestApp;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringTestApp.class)
+// See https://github.com/spring-projects/spring-boot/issues/33661
+@Ignore("Spring Boot Test is based on tomcat, so it needs Jakarta 6.x and we can't import it as test dep because we need the 5.x to compile SimpleFileIOErrorCode.java")
 public class S3TestIT extends S3TestITBase {
     // all test case are in the parent class.
 }
